830115ca8a2d94ee644e536872554f8eab77961e
chrisbriz <christopher.brizet@outlook.com>
2022-02-03 21:00:15 +0100

    Added components for simple board and fixed packages issues

--- examen/src/App.tsx
+++ examen/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import './App.css';
+// import './App.css';
+import { Board } from './components/board';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello world</h1>
+        <Board />
       </header>
     </div>
   );
